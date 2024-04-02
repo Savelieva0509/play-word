73459b750a65e4d6ff110857f7ffa8e4dc803544
Babenko Liudmyla <savelieva0509@gmail.com>
2024-04-03 01:42:23 +0300

    move wrapper letter

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
 import selectLetter from './selectLetter';
 import showEnteredText from './showEnteredText.js';
-import wrapLetters from './wrapLetters.js';
 
 const showTextBtn = document.querySelector('.button');
 showTextBtn.addEventListener('click', event => {
   showEnteredText();
-  wrapLetters();
   selectLetter();
 });
--- src/js/showEnteredText.js
+++ src/js/showEnteredText.js
@@ -3,7 +3,15 @@ function showEnteredText() {
   const textField = document.querySelector('.text-field');
 
   const enteredText = input.value.toLowerCase();
-  textField.textContent = enteredText;
+
+  textField.innerHTML = '';
+
+  for (let i = 0; i < enteredText.length; i++) {
+    const letter = document.createElement('span');
+    letter.textContent = enteredText[i];
+    letter.classList.add('letter');
+    textField.appendChild(letter);
+  }
 }
 
 export default showEnteredText;
